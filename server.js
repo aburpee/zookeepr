a5a2d3f988f394a418f622da912cad866e879169
bonobo <arburpee@gmail.com>
2022-02-08 22:03:00 -0800

    add heroku

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const { animals } = require('./data/animals');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const { animals } = require('./data/animals');
+
 const app = express();
-//parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-//parse incoming json data
-app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
-    let personalityTrainsArray = [];
-    //note that we save the animals array as filtered results here:
+    let personalityTraitsArray = [];
     let filteredResults = animalsArray;
+
     if (query.personalityTraits) {
-        //save personality traits as a dedicated array
-        //if personality trains is a string, place it into a new array and save
         if (typeof query.personalityTraits === 'string') {
             personalityTraitsArray = [query.personalityTraits];
         } else {
             personalityTraitsArray = query.personalityTraits;
         }
-        //loop through each trait in the personality traits array:
         personalityTraitsArray.forEach(trait => {
-            //check the trait against each animal in the filtered results array
-            //it is initially a copy of the animals array
-            //but here we update it for each trait in the for each loop
-            //for each trait being targeted by the filter, the filtered results
-            //array will then contain only the entries that contain the trait,
-            //so at the end we'll have an array of animlas that have every one
-            //of the traits when the for each loop is finished
             filteredResults = filteredResults.filter(
                 animal => animal.personalityTraits.indexOf(trait) !== -1
             );
@@ -47,38 +31,6 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-    
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-    //return finished code to post route for response
-    return animal;
-}
-
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
-}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -86,33 +38,9 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
-});
-
-app.post('/api/animals', (req, res) => {
-    //req.body is where our incoming content will be
-    //set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('the animal is not properly formatted');
-    } else {
-        //add animal to json file and animals array in this function
-        const animal = createNewAnimal(req.body, animals);
-        res.json(req.body);
-    }
-});
-
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-    res.json(result);
 })
 
-app.listen(PORT, () => {
-    console.log(`api server now on port ${PORT}`);
+app.listen(3001, () => {
+    console.log(`api server now on port 3001`);
 });
+
